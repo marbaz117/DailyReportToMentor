e0d759b9c54427d22cb094e064e31decf42ce441
Md Arbaz <143584027+mdarbaz1712@users.noreply.github.com>
2026-01-29 16:51:27 +0530

    Day 5

---
+++ JavaBasics/src/Main.java
@@ -0,0 +1,87 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//void main() {
+//    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+//    // to see how IntelliJ IDEA suggests fixing it.
+//    IO.println(String.format("Hello and welcome!"));
+//
+//    for (int i = 1; i <= 5; i++) {
+//        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+//        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+//        IO.println("i = " + i);
+//    }
+//}
+
+
+/******************************************************************************
+
+ Welcome to GDB Online.
+ GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+ C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+ Code, Compile, Run and Debug online from anywhere in world.
+
+ *******************************************************************************/
+public class Main
+{
+    // static String leftTrim(String s){
+    //     int i=0;
+    //     while(i<s.length()&&s.charAt(i)==' '){
+    //         i++;
+    //     }
+    //     return s.substring(i);
+    // }
+    public static void main(String[] args) {
+        // final int a;
+        // a=10;
+        // a=90;
+// 	  char a1=99;//ouput:c
+// 	  float f=9.988f;
+// 	  double d=9.564445;
+// 	  long l=999999L;
+// 	  var cars = new ArrayList<String>();//valid
+// 	  float f=9.21131f;
+// 	  int f1=(int)f;//9
+// 	  f=(int)f;//9.0
+//    double d=(double) 432/500 *100;//86.4
+//   int x=8*7/4;//14
+// 	  String s="abfjsjhsbr\\jksrn";
+// 	  String s1="     abfjsjhsbrjksrn     ";
+// 	  s="10";
+// 	  s=s.toUpperCase();
+// 	  System.out.println(s.toUpperCase());
+// 	  System.out.println(s);
+        // int a=10;
+        // double a=90,b=-91.0;
+        // System.out.println(Math.min(a,b));
+        // System.out.println(Math.sqrt(a));
+        // System.out.println(Math.round(b));
+        // System.out.println(Math.pow(a,2));
+        // System.out.println(Math.ceil(b));
+
+        // System.out.println(leftTrim(s1)+"Hii");
+        // System.out.println(s.concat(s1));
+        // System.out.println(s==a);
+        // System.out.println(System.identityHashCode(s1));
+        // 		System.out.println(x);
+
+        // String x=(20<10)?"True":(10>20)?"False":"True";
+        // System.out.println(x);
+
+        // int day=6;
+        // switch(day){
+        //     case 5:
+        //         System.out.println("Hello");
+        //     default:
+        //         System.out.println("Default");
+        //         break;
+        //     case 6:
+        //         System.out.println("Bye!!");
+        // }
+
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        System.out.println(cars.length);
+
+
+
+    }
+}
